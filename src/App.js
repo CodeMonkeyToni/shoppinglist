82f54eecb6ad91dd022829c33484d06260cfe84c
CodeMonkeyToni <toni.koljonen@gmail.com>
2024-09-27 15:16:14 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import cart from './images/cart.svg';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 function App() {
+  const appSettings = {
+    databaseURL: "https://realtime-database-ebf3b-default-rtdb.europe-west1.firebasedatabase.app/"
+  }
+
+  const [itemToBeAdded, setItemToBeAdded] = React.useState("");
+  const [shoppingList, setShoppingList] = React.useState([]);
+  
+  const app = initializeApp(appSettings);
+  const database = getDatabase(app);
+  const shoppingListInDB = ref(database, "shoppingList");
+
+  React.useEffect(() => {
+    onValue(shoppingListInDB, function(snapshot) {
+      const data = snapshot.val();
+      if (data) {
+        setShoppingList(Object.entries(data))
+      } else {
+        setShoppingList([])
+      }
+    })
+  }, []);
+
+  function clearItemToBeAdded() {
+    setItemToBeAdded("");
+  }
+
+  function addToCart() {
+    if (itemToBeAdded.trim() !== "") {
+      push(shoppingListInDB, itemToBeAdded);
+      clearItemToBeAdded();
+    }
+  }
+
+function handleChange(event) {
+  const value = event.target.value;
+  setItemToBeAdded(value);
+}
+
+function removeItem(event) {
+  const id = event.target.id;
+  const location = ref(database, `shoppingList/${id}`);
+  remove(location);
+}
+
+const items = shoppingList.map(item => {
+  return <button className="item" onClick={removeItem} id={item[0]} key={item[0]}>{item[1]}</button>
+});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="site">
+      <div className="main">
+        <img src={cart} alt="" className="image" />
+        <input className="input-field" onChange={handleChange} value={itemToBeAdded} type="text" placeholder="Bread"></input>
+        <button className="add-button" onClick={addToCart}>Add to cart</button>
+        <div className="item-list">
+          {items}
+        </div>
+      </div>
     </div>
   );
 }
